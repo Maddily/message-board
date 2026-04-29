a64d30be9c9e11b401796a0e37b5c093d91c9aa1
Maddily <115709272+Maddily@users.noreply.github.com>
2026-04-29 20:41:57 +0300

    Pass generateRandomPastelColor to message view

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,6 +1,6 @@
 import Router from "express";
 import { messages } from "./indexRouter.js";
-import { generateRandomColor } from "../utils.js";
+import { generateRandomColor, generateRandomPastelColor } from "../utils.js";
 
 const messageRouter = Router();
 
@@ -8,7 +8,7 @@ messageRouter.get("/:messageId", (req, res) => {
   const { messageId } = req.params;
   const message = messages.find((message) => message.id === messageId);
 
-  res.render("message", { message, generateRandomColor });
+  res.render("message", { message, generateRandomColor, generateRandomPastelColor });
 });
 
 export default messageRouter;
