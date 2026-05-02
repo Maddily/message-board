007bea50db4d54e580e74d0e249b1549996808ad
Maddily <115709272+Maddily@users.noreply.github.com>
2026-05-02 14:25:34 +0300

    Pass generateRandomPastelColor and add more messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,7 @@
 import Router from "express";
 import { v4 as uuidv4 } from "uuid";
 import crypto from "crypto";
-import { formatDate, generateRandomColor } from "../utils.js";
+import { formatDate, generateRandomColor, generateRandomPastelColor } from "../utils.js";
 
 globalThis.crypto = crypto;
 const indexRouter = Router();
@@ -19,10 +19,56 @@ const messages = [
     user: "Espresso",
     added: formatDate(new Date()),
   },
+  {
+    id: uuidv4(),
+    text: "Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii",
+    user: "Mimi",
+    added: formatDate(new Date()),
+  },
+  {
+    id: uuidv4(),
+    text: "Coffee is delicious",
+    user: "Latte",
+    added: formatDate(new Date()),
+  },
+  {
+    id: uuidv4(),
+    text: "Hehehe",
+    user: "Squirrel",
+    added: formatDate(new Date()),
+  },
+  {
+    id: uuidv4(),
+    text: "Helloooooooooooooooooooooooooo",
+    user: "Rabbit",
+    added: formatDate(new Date()),
+  },
+  {
+    id: uuidv4(),
+    text: "Hello again!!!!!!!!!!!!!!!!!",
+    user: "Mimi",
+    added: formatDate(new Date()),
+  },
+  {
+    id: uuidv4(),
+    text: "Good morning",
+    user: "Munch",
+    added: formatDate(new Date()),
+  },
+  {
+    id: uuidv4(),
+    text: "........................................................................................................",
+    user: "Susu",
+    added: formatDate(new Date()),
+  },
 ];
 
-indexRouter.get("/", (req, res) => res.render("index", { messages, generateRandomColor }));
-indexRouter.get("/new", (req, res) => res.render("form"));
+indexRouter.get("/", (req, res) =>
+  res.render("index", { messages, generateRandomColor, generateRandomPastelColor })
+);
+indexRouter.get("/new", (req, res) =>
+  res.render("form", { generateRandomColor, generateRandomPastelColor })
+);
 indexRouter.post("/new", (req, res) => {
   const message = {
     id: uuidv4(),
